c971e633425c9c837c5281c826b1d7ea1c17d31a
PrithvirajChauhan1 <70701432+PrithvirajChauhan1@users.noreply.github.com>
2021-10-13 13:34:37 +0530

    Deploment process 4

---
+++ src/components/Footers/DarkFooter.js
@@ -0,0 +1,55 @@
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+function DarkFooter() {
+	return (
+		<footer className="footer" data-background-color="black" id='contact'>
+			<Container>
+				<nav>
+					<ul>
+						<li>
+							<a
+								href="https://www.creative-tim.com?ref=nukr-dark-footer"
+								target="_blank"
+							>
+								Team Dendo
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.linkedin.com/company/74050850"
+								target="_blank"
+							>
+								About Us
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.linkedin.com/feed/update/urn:li:activity:6815952720344715264"
+								target="_blank"
+							>
+								Blog
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<div className="copyright" id="copyright">
+					© {new Date().getFullYear()}, Designed by{" "}
+					
+					Dendo .Inc
+
+					. Coded by{" "}
+					
+						Team Dendo
+					
+					.
+				</div>
+			</Container>
+		</footer>
+	);
+}
+
+export default DarkFooter;
